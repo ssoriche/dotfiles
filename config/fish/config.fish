142f681d7d1e3244a7c70af46ddd4eb5d9226732
Shawn Sorichetti <ssoriche@gmail.com>
2017-05-11 21:23:39 -0400

    Add fish shell configuration
    This is the start of getting up and running with fish shell.

---
+++ config/fish/config.fish
@@ -0,0 +1,15 @@
+# THEME PURE #
+set fish_function_path $HOME/.config/fish/functions/theme-pure $fish_function_path
+
+function fish_mode_prompt
+# Turn off mode indictor
+end
+
+function custom_key_binds
+    fish_vi_key_bindings
+
+    bind -M insert \ca beginning-of-line
+    bind -M insert \ce end-of-line
+    bind -M insert \cf accept-autosuggestion
+end
+set -g fish_key_bindings custom_key_binds
