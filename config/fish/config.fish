2b8a08b8c3663754b0d1fc78dbb8869994518e87
Shawn Sorichetti <shawn@coloredblocks.com>
2017-05-17 22:10:05 -0400

    Add setting of PATH
    This is different than what I'm used to for setting the PATH but it is a
    lot nicer and sets the PATH variable based on the path existing.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,3 +18,26 @@ set -g fish_key_bindings custom_key_binds
 alias g git
 alias vim nvim
 alias vi nvim
+
+# Set path
+function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
+    if test -d $argv[1]
+        if not contains $argv[1] $PATH
+            set -gx PATH "$argv[1]" $PATH
+        end
+    end
+end
+
+set -gx PATH "/sbin"
+prepend_to_path "/bin"
+prepend_to_path "/usr/sbin"
+prepend_to_path "/usr/bin"
+prepend_to_path "/usr/local/bin"
+prepend_to_path "/usr/local/opt/git/share/git-core/contrib/diff-highlight"
+prepend_to_path "/usr/local/opt/groovy/libexec/bin"
+prepend_to_path "/usr/local/gradle/bin"
+prepend_to_path "/usr/local/MacGPG2/bin"
+prepend_to_path "$HOME/bin"
+
+# Configure plenv
+status --is-interactive; and . (plenv init -|psub)
