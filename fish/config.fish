9298498540f37598f15ea8775280ed738aa8bff9
Shawn Sorichetti <shawn@coloredblocks.com>
2020-06-15 16:37:53 -0400

    Add fzf colouring and prompt theming
    I just want it to be pretty.

--- fish/config.fish
+++ fish/config.fish
@@ -84,3 +84,5 @@ eval (direnv hook fish)
 kitty + complete setup fish | source
 set -gx PATH $HOME/.anyenv/bin $PATH
 source (anyenv init - fish|psub)
+
+set -gx FZF_DEFAULT_OPTS '--color fg:188,bg:233,hl:103,fg+:222,bg+:234,hl+:104 --color info:183,prompt:110,spinner:107,pointer:167,marker:215'
