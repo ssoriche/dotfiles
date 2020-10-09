8f1d7bd8a252643ff32aa23526e1c43358556609
Shawn Sorichetti <shawns@ziprecruiter.com>
2020-10-09 11:17:57 -0400

    Add support for asdf
    asdf provides anyenv type version switching but for applications. Check
    if it is installed in the usual location before sourcing environment.

--- fish/config.fish
+++ fish/config.fish
@@ -76,3 +76,6 @@ if command -v anyenv > /dev/null
 end
 
 set -gx FZF_DEFAULT_OPTS '--color fg:188,bg:233,hl:103,fg+:222,bg+:234,hl+:104 --color info:183,prompt:110,spinner:107,pointer:167,marker:215'
+if test -e /usr/local/opt/asdf/asdf.fish
+  source /usr/local/opt/asdf/asdf.fish
+end
