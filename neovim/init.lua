cdb451a9e9522dd80b025fcfe690f82809a5c139
Shawn Sorichetti <shawn@coloredblocks.com>
2021-02-20 20:15:03 -0500

    Formatting from luaformat
    Now that this is all working with the lsp, my configuration files are
    automatically formated for me.

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,15 +1,16 @@
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
-local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
-local g = vim.g      -- a table to access global variables
+local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g -- a table to access global variables
 local execute = vim.api.nvim_command
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 
 g.mapleader = " "
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute 'packadd packer.nvim'
+    execute('!git clone https://github.com/wbthomason/packer.nvim ' ..
+                install_path)
+    execute 'packadd packer.nvim'
 end
 
 require('plugins')
@@ -19,40 +20,42 @@ require("treesitter")
 local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
 local function opt(scope, key, value)
-  scopes[scope][key] = value
-  if scope ~= 'o' then scopes['o'][key] = value end
+    scopes[scope][key] = value
+    if scope ~= 'o' then scopes['o'][key] = value end
 end
 
 local indent = 2
-cmd 'colorscheme halcyon'                              -- Put your favorite colorscheme here
-opt('b', 'expandtab', true)                           -- Use spaces instead of tabs
-opt('b', 'shiftwidth', indent)                        -- Size of an indent
-opt('b', 'smartindent', true)                         -- Insert indents automatically
-opt('b', 'tabstop', indent)                           -- Number of spaces tabs count for
-opt('o', 'completeopt', 'menuone,noinsert,noselect')  -- Completion options (for deoplete)
-opt('o', 'hidden', true)                              -- Enable modified buffers in background
-opt('o', 'ignorecase', true)                          -- Ignore case
-opt('o', 'joinspaces', false)                         -- No double spaces with join after a dot
-opt('o', 'scrolloff', 4 )                             -- Lines of context
-opt('o', 'shiftround', true)                          -- Round indent
-opt('o', 'sidescrolloff', 8 )                         -- Columns of context
-opt('o', 'smartcase', true)                           -- Don't ignore case with capitals
-opt('o', 'splitbelow', true)                          -- Put new windows below current
-opt('o', 'splitright', true)                          -- Put new windows right of current
-opt('o', 'termguicolors', true)                       -- True color support
-opt('o', 'wildmode', 'list:longest')                  -- Command-line completion mode
+cmd 'colorscheme halcyon' -- Put your favorite colorscheme here
+opt('b', 'expandtab', true) -- Use spaces instead of tabs
+opt('b', 'shiftwidth', indent) -- Size of an indent
+opt('b', 'smartindent', true) -- Insert indents automatically
+opt('b', 'tabstop', indent) -- Number of spaces tabs count for
+opt('o', 'completeopt', 'menuone,noinsert,noselect') -- Completion options (for deoplete)
+opt('o', 'hidden', true) -- Enable modified buffers in background
+opt('o', 'ignorecase', true) -- Ignore case
+opt('o', 'joinspaces', false) -- No double spaces with join after a dot
+opt('o', 'scrolloff', 4) -- Lines of context
+opt('o', 'shiftround', true) -- Round indent
+opt('o', 'sidescrolloff', 8) -- Columns of context
+opt('o', 'smartcase', true) -- Don't ignore case with capitals
+opt('o', 'splitbelow', true) -- Put new windows below current
+opt('o', 'splitright', true) -- Put new windows right of current
+opt('o', 'termguicolors', true) -- True color support
+opt('o', 'wildmode', 'list:longest') -- Command-line completion mode
 -- opt('w', 'list', true)                                -- Show some invisible characters (tabs...)
-opt('w', 'number', true)                              -- Print line number
-opt('w', 'relativenumber', true)                      -- Relative line numbers
-opt('w', 'wrap', false)                               -- Disable line wrap
+opt('w', 'number', true) -- Print line number
+opt('w', 'relativenumber', true) -- Relative line numbers
+opt('w', 'wrap', false) -- Disable line wrap
 
 local lualine = require('lualine')
 lualine.status()
+lualine.options.theme = 'jellybeans'
+lualine.extensions = {'fzf'}
 
 local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    local options = {noremap = true}
+    if opts then options = vim.tbl_extend('force', options, opts) end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 map('', '<leader>y', '"+y')
@@ -62,23 +65,20 @@ map('', '<leader> ', '<cmd>Buffers<CR>')
 --[[ local ts = require 'nvim-treesitter.configs'
 ts.setup {ensure_installed = 'maintained', highlight = {enable = true}} ]]
 
-
 require'nvim-web-devicons'.setup {
- -- your personnal icons can go here (to override)
- -- DevIcon will be appended to `name`
- -- override = {
- --  zsh = {
- --    icon = "",
- --    color = "#428850",
- --    name = "Zsh"
- --  }
- -- };
- -- globally enable default icons (default to false)
- -- will get overriden by `get_icons` option
- default = true;
+    -- your personnal icons can go here (to override)
+    -- DevIcon will be appended to `name`
+    -- override = {
+    --  zsh = {
+    --    icon = "",
+    --    color = "#428850",
+    --    name = "Zsh"
+    --  }
+    -- };
+    -- globally enable default icons (default to false)
+    -- will get overriden by `get_icons` option
+    default = true,
 }
 
-local saga = require'lspsaga'
-saga.init_lsp_saga {
-  border_style = 1
-}
+local saga = require 'lspsaga'
+saga.init_lsp_saga {border_style = 1}
