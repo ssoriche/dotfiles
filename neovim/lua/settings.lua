cc32cafaba137fb645189918748cc8895467531c
Shawn Sorichetti <shawn@coloredblocks.com>
2021-03-02 17:53:34 -0500

    Migrate to using an opt object
    It seems there is an issue with setting options that some are buffer and
    global and some are window and global, but setting buffer or window
    alone is not enough to have them be applied to new  buffers. There's
    work being done to fix that but until the PR is merged, this workaround
    is in place.
    
    https://www.reddit.com/r/neovim/comments/l6a8z8/is_it_just_me_or_some_options_dont_work_in_lua_yet/

--- neovim/lua/settings.lua
+++ neovim/lua/settings.lua
@@ -1,30 +1,41 @@
-local o = vim.o -- Global options
-local wo = vim.wo -- Window options
-local bo = vim.bo -- Buffer options
+-- TODO: Remove when https://github.com/neovim/neovim/pull/13479 lands
+local opts_info = vim.api.nvim_get_all_options_info()
+local opt = setmetatable({}, {
+  __index = vim.o,
+  __newindex = function(_, key, value)
+    vim.o[key] = value
+    local scope = opts_info[key].scope
+    if scope == "win" then
+      vim.wo[key] = value
+    elseif scope == "buf" then
+      vim.bo[key] = value
+    end
+  end,
+})
 
 local indent = 2
 
 -- Buffer
-bo.expandtab = true -- Use spaces instead of tabs
-bo.shiftwidth = indent -- Size of an indent
-bo.smartindent = true -- Insert indents automatically
-bo.tabstop = indent -- Number of spaces tabs count for
+opt.expandtab = true -- Use spaces instead of tabs
+opt.shiftwidth = indent -- Size of an indent
+opt.smartindent = true -- Insert indents automatically
+opt.tabstop = indent -- Number of spaces tabs count for
 
 -- Global
-o.completeopt = 'menuone,noinsert,noselect' -- Completion options (for deoplete)
-o.hidden = true -- Enable modified buffers in background
-o.ignorecase = true -- Ignore case
-o.joinspaces = false -- No double spaces with join after a dot
-o.scrolloff = 4 -- Lines of context
-o.shiftround = true -- Round indent
-o.sidescrolloff = 8 -- Columns of context
-o.smartcase = true -- Don't ignore case with capitals
-o.splitbelow = true -- Put new windows below current
-o.splitright = true -- Put new windows right of current
-o.termguicolors = true -- True color support
-o.wildmode = 'list:longest' -- Command-line completion mode
+opt.completeopt = 'menuone,noinsert,noselect' -- Completion options (for deoplete)
+opt.hidden = true -- Enable modified buffers in background
+opt.ignorecase = true -- Ignore case
+opt.joinspaces = false -- No double spaces with join after a dot
+opt.scrolloff = 4 -- Lines of context
+opt.shiftround = true -- Round indent
+opt.sidescrolloff = 8 -- Columns of context
+opt.smartcase = true -- Don't ignore case with capitals
+opt.splitbelow = true -- Put new windows below current
+opt.splitright = true -- Put new windows right of current
+opt.termguicolors = true -- True color support
+opt.wildmode = 'list:longest' -- Command-line completion mode
 
 -- Window
-wo.number = true -- Print line number
-wo.relativenumber = true -- Relative line numbers
-wo.wrap = false -- Disable line wrap
+opt.number = true -- Print line number
+opt.relativenumber = true -- Relative line numbers
+opt.wrap = false -- Disable line wrap
