9f78ce934f80f1809c288e6729e68c56eb52678f
Shawn Sorichetti <shawn@coloredblocks.com>
2021-03-17 14:11:43 -0400

    Change to use fish_add_path
    Fish 3.2.0 introduced fish_add_path to add to the begin of the path
    environment variable without deplication, the same thing that
    prepend_to_path was attempting to do, but this is built in, let's use
    it.

--- fish/config.fish
+++ fish/config.fish
@@ -35,28 +35,20 @@ else if command -s vim > /dev/null
   set -gx VISUAL vim
 end
 
-# Set path
-function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
-    if test -d $argv[1]
-        if not contains $argv[1] $PATH
-            set -gx PATH "$argv[1]" $PATH
-        end
-    end
-end
-
-set -gx PATH "/sbin"
-prepend_to_path "/bin"
-prepend_to_path "/usr/sbin"
-prepend_to_path "/usr/bin"
-prepend_to_path "/usr/local/sbin"
-prepend_to_path "/usr/local/bin"
-prepend_to_path "/usr/local/opt/git/share/git-core/contrib/diff-highlight"
-prepend_to_path "/usr/local/opt/groovy/libexec/bin"
-prepend_to_path "/usr/local/gradle/bin"
-prepend_to_path "/usr/local/MacGPG2/bin"
-prepend_to_path "$HOME/bin"
-prepend_to_path "$HOME/.pgenv/bin"
-prepend_to_path "$HOME/.pgenv/pgsql/bin"
+fish_add_path "/sbin"
+fish_add_path "/bin"
+fish_add_path "/usr/sbin"
+fish_add_path "/usr/bin"
+fish_add_path "/usr/local/sbin"
+fish_add_path "/usr/local/bin"
+fish_add_path "/usr/local/opt/git/share/git-core/contrib/diff-highlight"
+fish_add_path "/usr/local/opt/groovy/libexec/bin"
+fish_add_path "/usr/local/gradle/bin"
+fish_add_path "/usr/local/MacGPG2/bin"
+fish_add_path "$HOME/bin"
+fish_add_path "$HOME/.pgenv/bin"
+fish_add_path "$HOME/.pgenv/pgsql/bin"
+fish_add_path "$HOME/go/bin"
 
 set -gx LESS "-F -X -R"
 if command -s /usr/local/bin/bat > /dev/null
@@ -72,7 +64,7 @@ eval (direnv hook fish)
 
 kitty + complete setup fish | source
 if command -v anyenv > /dev/null
-  set -gx PATH $HOME/.anyenv/bin $PATH
+  fish_add_path $HOME/.anyenv/bin
   source (anyenv init - fish|psub)
 end
 
