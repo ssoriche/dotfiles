1a35da34607eec685c1d39445943cc4a5749e823
Shawn Sorichetti <shawn@coloredblocks.com>
2021-03-20 20:55:26 -0400

    Run through lua format
    This is just tidying the previous config with lua format.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,29 +1,65 @@
 local wezterm = require 'wezterm';
 return {
-  font_size = 12.0,
-  font = wezterm.font("JetBrainsMono Nerd Font"),
-  allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
-  color_scheme = "Jellybeans",
+    font_size = 12.0,
+    font = wezterm.font("JetBrainsMono Nerd Font"),
+    allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
+    color_scheme = "Jellybeans",
 
-  initial_cols = 120,
-  initial_rows = 50,
+    initial_cols = 120,
+    initial_rows = 50,
 
-  window_padding = {
-    bottom = 4,
-    left = 2,
-    right = 2,
-  },
-  leader = { key="s", mods="CTRL" },
-  keys = {
-    { key = "-", mods = "LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}} },
-    { key = "\\", mods = "LEADER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}} },
-    { key = "z", mods = "LEADER", action="TogglePaneZoomState" },
-    { key = "c", mods = "LEADER", action=wezterm.action{SpawnTab="CurrentPaneDomain"} },
-    { key = "n", mods = "LEADER", action=wezterm.action{ActivateTabRelative=1} },
-    { key = "p", mods = "LEADER", action=wezterm.action{ActivateTabRelative=-1} },
-    { key = "h", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Left"} },
-    { key = "l", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Right"} },
-    { key = "j", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Down"} },
-    { key = "k", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Up"} },
-  }
+    window_padding = {bottom = 4, left = 2, right = 2},
+    leader = {key = "s", mods = "CTRL"},
+    keys = {
+        {
+            key = "-",
+            mods = "LEADER",
+            action = wezterm.action {
+                SplitVertical = {domain = "CurrentPaneDomain"},
+            },
+        },
+        {
+            key = "\\",
+            mods = "LEADER",
+            action = wezterm.action {
+                SplitHorizontal = {domain = "CurrentPaneDomain"},
+            },
+        },
+        {key = "z", mods = "LEADER", action = "TogglePaneZoomState"},
+        {
+            key = "c",
+            mods = "LEADER",
+            action = wezterm.action {SpawnTab = "CurrentPaneDomain"},
+        },
+        {
+            key = "n",
+            mods = "LEADER",
+            action = wezterm.action {ActivateTabRelative = 1},
+        },
+        {
+            key = "p",
+            mods = "LEADER",
+            action = wezterm.action {ActivateTabRelative = -1},
+        },
+        {
+            key = "h",
+            mods = "LEADER",
+            action = wezterm.action {ActivatePaneDirection = "Left"},
+        },
+        {
+            key = "l",
+            mods = "LEADER",
+            action = wezterm.action {ActivatePaneDirection = "Right"},
+        },
+        {
+            key = "j",
+            mods = "LEADER",
+            action = wezterm.action {ActivatePaneDirection = "Down"},
+        },
+        {
+            key = "k",
+            mods = "LEADER",
+            action = wezterm.action {ActivatePaneDirection = "Up"},
+        },
+    },
 }
