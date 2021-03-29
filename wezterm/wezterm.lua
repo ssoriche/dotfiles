62eac9e1e8bcbd9328590b28f67efed442e7fdee
Shawn Sorichetti <shawn@coloredblocks.com>
2021-03-29 09:23:20 -0400

    Found out wezterm has a material theme too!
    Love this color scheme in neovim, and just tried it on the command line,
    very nice there too.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ return {
     font_size = 12.0,
     font = wezterm.font("JetBrainsMono Nerd Font"),
     allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
-    color_scheme = "Jellybeans",
+    color_scheme = "MaterialOcean",
 
     initial_cols = 120,
     initial_rows = 50,
