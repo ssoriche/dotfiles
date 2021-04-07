7203ecc3afc90814a52d2b8273aa0e91e4eaf299
Shawn Sorichetti <shawn@coloredblocks.com>
2021-04-07 12:48:39 -0400

    Add documentation for lua-format install
    Took me entirely too long to figure out where lua-format came from and
    how to install it.

--- neovim/lua/lsp/efm/luafmt.lua
+++ neovim/lua/lsp/efm/luafmt.lua
@@ -1,4 +1,6 @@
+-- In order to function lua-format command must be installed via:
+-- luarocks install --server=https://luarocks.org/dev luaformatter
 return {
     formatCommand = "lua-format --chop-down-kv-table --chop-down-table --extra-sep-at-table-end",
-    formatStdin = true
+    formatStdin = true,
 }
