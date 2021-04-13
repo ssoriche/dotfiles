80ebd47e3dee04c6e3dc3408fbc67ee7810cc34f
Shawn Sorichetti <shawn@coloredblocks.com>
2021-04-13 17:41:40 -0400

    Add ftplugin for gitcommit
    Getting tired of everything being displayed in my git commit messages,
    also that spelling isn't enabled. Adding a gitcommit.lua file to set
    these settings when editing a gitcommit.

---
+++ neovim/ftplugin/gitcommit.lua
@@ -0,0 +1,17 @@
+-- TODO: Remove when https://github.com/neovim/neovim/pull/13479 lands
+local opts_info = vim.api.nvim_get_all_options_info()
+local opt = setmetatable({}, {
+    __index = vim.o,
+    __newindex = function(_, key, value)
+        vim.o[key] = value
+        local scope = opts_info[key].scope
+        if scope == "win" then
+            vim.wo[key] = value
+        elseif scope == "buf" then
+            vim.bo[key] = value
+        end
+    end,
+})
+
+opt.spell = true
+opt.foldmethod = 'syntax'
