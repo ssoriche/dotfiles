183e9055d5e4651696059691eddd79ef58427ab6
Shawn Sorichetti <shawn@coloredblocks.com>
2021-04-29 15:17:38 -0400

    Add which-key and  lsp-trouble plugins
    These plugins are by folke and have been very highly touted. I've used
    both of them a bit throughout the day and they seem to work nicely.

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -46,4 +46,27 @@ return require('packer').startup(function()
 
     use 'JoosepAlviste/nvim-ts-context-commentstring'
 
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end,
+    }
+
+    use {
+        "folke/lsp-trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end,
+    }
+
 end)
