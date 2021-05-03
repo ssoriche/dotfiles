06ebb0abbae828f36c99dee2a3240afeb212f609
Shawn Sorichetti <shawn@coloredblocks.com>
2021-05-03 10:59:24 -0400

    Add settings for lualine
    Seems that they've removed the defaults that were normally set if any
    options were being set. I want my theme, so I need to specify the
    default sections as part of the options.

--- neovim/init.lua
+++ neovim/init.lua
@@ -22,7 +22,25 @@ g.material_italics = 1
 cmd 'colorscheme material' -- Put your favorite colorscheme here
 
 local lualine = require('lualine')
-lualine.setup {options = {theme = 'material-nvim'}, extensions = {'fzf'}}
+lualine.setup {
+    options = {theme = 'material-nvim'},
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch'},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'},
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {},
+    },
+}
 
 --[[ local ts = require 'nvim-treesitter.configs'
 ts.setup {ensure_installed = 'maintained', highlight = {enable = true}} ]]
