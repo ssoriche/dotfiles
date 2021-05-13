2a69c2c69adfc5a2d2b130c1e2f02295224cfe95
Shawn Sorichetti <shawn@coloredblocks.com>
2021-05-13 17:05:58 -0400

    Enable spelling module in which-key
    Recent update to which-key allows for z= to bring up the which-key
    popup with spelling suggestions. So much nicer than the default z=.

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -51,9 +51,7 @@ return require('packer').startup(function()
         "folke/which-key.nvim",
         config = function()
             require("which-key").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
+                plugins = {spelling = {enabled = true, suggestions = 20}},
             }
         end,
     }
