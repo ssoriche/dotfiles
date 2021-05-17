b0508792f56ab6ee7263444d4847ebb115126b9e
Shawn Sorichetti <shawn@coloredblocks.com>
2021-05-17 16:14:57 -0400

    Add todo-comments plugin
    Starting to see more of these in use with work code, want to be able to
    find them easily, mostly because they're going to have to be resolved at
    some point.

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -68,4 +68,15 @@ return require('packer').startup(function()
         end,
     }
 
+    use {
+        "folke/todo-comments.nvim",
+        config = function()
+            require("todo-comments").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end,
+    }
+
 end)
