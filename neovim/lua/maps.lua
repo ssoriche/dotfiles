179b65514c056e84cd1aad1a2b2ade839b61bb0c
Shawn Sorichetti <shawn@coloredblocks.com>
2021-05-18 15:54:50 -0400

    Add configuration for Telescope buffers command
    These configuration options allow telescope to show all buffers that are
    open (so recovered from a session) not just the ones opened by
    Telescope.
    
    Also to sort them in the order of last use, which allows me to easily
    switch between recently used files quickly.

--- neovim/lua/maps.lua
+++ neovim/lua/maps.lua
@@ -13,8 +13,10 @@ map('v', 'Q', ':norm @q<cr>', options)
 map('n', '<bs>', '<c-^>', options)
 
 -- Telescope
-local builtin = "<cmd>lua require('telescope.builtin')"
-map('n', '<leader> ', builtin .. '.buffers()<CR>', options)
+local builtin = "<cmd>lua require'telescope.builtin'"
+map('n', '<leader> ',
+    builtin .. '.buffers{ show_all_buffers = true, sort_lastused = true}<CR>',
+    options)
 map('n', '<leader>ff', builtin .. '.find_files()<CR>', options)
 map('n', '<leader>of', builtin .. '.oldfiles()<CR>', options)
 
