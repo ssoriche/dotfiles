9e23a70e739f1bde50079ec40b7f7b0d5d1fddea
Shawn Sorichetti <shawn@coloredblocks.com>
2021-06-08 09:22:38 -0400

    Switch to using vim.opt for option setting
    The PR finally landed and the hack to set the appropriate scope on vim
    options has landed. Start using that instead.

--- neovim/lua/settings.lua
+++ neovim/lua/settings.lua
@@ -1,44 +1,29 @@
--- TODO: Remove when https://github.com/neovim/neovim/pull/13479 lands
-local opts_info = vim.api.nvim_get_all_options_info()
-local opt = setmetatable({}, {
-    __index = vim.o,
-    __newindex = function(_, key, value)
-        vim.o[key] = value
-        local scope = opts_info[key].scope
-        if scope == "win" then
-            vim.wo[key] = value
-        elseif scope == "buf" then
-            vim.bo[key] = value
-        end
-    end,
-})
-
 local indent = 2
 
 -- Buffer
-opt.expandtab = true -- Use spaces instead of tabs
-opt.shiftwidth = indent -- Size of an indent
-opt.smartindent = true -- Insert indents automatically
-opt.tabstop = indent -- Number of spaces tabs count for
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = indent -- Size of an indent
+vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.tabstop = indent -- Number of spaces tabs count for
 
 -- Global
-opt.completeopt = 'menuone,noinsert,noselect' -- Completion options (for deoplete)
-opt.hidden = true -- Enable modified buffers in background
-opt.ignorecase = true -- Ignore case
-opt.joinspaces = false -- No double spaces with join after a dot
-opt.scrolloff = 4 -- Lines of context
-opt.shiftround = true -- Round indent
-opt.sidescrolloff = 8 -- Columns of context
-opt.smartcase = true -- Don't ignore case with capitals
-opt.splitbelow = true -- Put new windows below current
-opt.splitright = true -- Put new windows right of current
-opt.termguicolors = true -- True color support
-opt.wildmode = 'list:longest' -- Command-line completion mode
+vim.opt.completeopt = 'menuone,noinsert,noselect' -- Completion options (for deoplete)
+vim.opt.hidden = true -- Enable modified buffers in background
+vim.opt.ignorecase = true -- Ignore case
+vim.opt.joinspaces = false -- No double spaces with join after a dot
+vim.opt.scrolloff = 4 -- Lines of context
+vim.opt.shiftround = true -- Round indent
+vim.opt.sidescrolloff = 8 -- Columns of context
+vim.opt.smartcase = true -- Don't ignore case with capitals
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.termguicolors = true -- True color support
+vim.opt.wildmode = 'list:longest' -- Command-line completion mode
 
 -- Window
-opt.number = true -- Print line number
-opt.relativenumber = true -- Relative line numbers
-opt.wrap = false -- Disable line wrap
+vim.opt.number = true -- Print line number
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.wrap = false -- Disable line wrap
 
-opt.foldmethod = 'expr'
-opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
