aa40bcc57865088b60e060e1af98420754c23cbd
Shawn Sorichetti <shawn@coloredblocks.com>
2021-09-05 16:12:21 -0400

    feat: migrate to nvim-cmp
    This module will replace nvim-compe in the future, doing the migration
    now as others have already done it.

--- neovim/init.lua
+++ neovim/init.lua
@@ -117,7 +117,7 @@ require'gitsigns'.setup({
 })
 
 require'nvim-autopairs'.setup()
-require("nvim-autopairs.completion.compe").setup(
+require("nvim-autopairs.completion.cmp").setup(
     {
         map_cr = true, --  map <CR> on insert mode
         map_complete = true, -- it will auto insert `(` after select function or method item
