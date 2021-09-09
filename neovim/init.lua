a702c2257496f46764344d6b9a467d3be13758b0
Shawn Sorichetti <shawn@coloredblocks.com>
2021-09-09 10:23:20 -0400

    cleanup: Migrate nvim-web-devicons to new layout

--- neovim/init.lua
+++ neovim/init.lua
@@ -42,21 +42,6 @@ require('kommentary.config').configure_language('typescriptreact', {
     end,
 })
 
-require'nvim-web-devicons'.setup {
-    -- your personnal icons can go here (to override)
-    -- DevIcon will be appended to `name`
-    -- override = {
-    --  zsh = {
-    --    icon = "",
-    --    color = "#428850",
-    --    name = "Zsh"
-    --  }
-    -- };
-    -- globally enable default icons (default to false)
-    -- will get overriden by `get_icons` option
-    default = true,
-}
-
 local actions = require('telescope.actions')
 require'telescope'.setup {
     config = {
