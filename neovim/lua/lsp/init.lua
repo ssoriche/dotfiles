826746460f2e75ddc4b9a890873404a14657f205
Shawn Sorichetti <shawn@coloredblocks.com>
2021-09-09 10:05:55 -0400

    cleanup: Migrate cmp to new layout
    This takes lspkind along with it as they really do go together.

--- neovim/lua/lsp/init.lua
+++ neovim/lua/lsp/init.lua
@@ -2,32 +2,6 @@ local lspconfig = require "lspconfig"
 
 local map = vim.api.nvim_set_keymap
 
-local lspkind = require('lspkind')
-local cmp = require('cmp')
-cmp.setup {
-    snippet = {expand = function(args) vim.fn["vsnip#anonymous"](args.body) end},
-    sources = {
-        {name = 'path'},
-        {name = 'buffer'},
-        {name = 'nvim_lsp'},
-        {name = 'nvim_lua'},
-    },
-    mapping = {
-        ['<CR>'] = cmp.mapping.confirm({
-            -- TODO: I may infact what Insert here, sometimes a variable gets
-            -- replaced when completing and I dont' want that
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        }),
-    },
-    formatting = {
-        format = function(entry, vim_item)
-            vim_item.kind = lspkind.presets.default[vim_item.kind]
-            return vim_item
-        end,
-    },
-}
-
 -- Setup buffer configuration (nvim-lua source only enables in Lua filetype).
 vim.api.nvim_command([[autocmd FileType lua lua require'cmp'.setup.buffer {
 sources = {
---
+++ neovim/lua/plugins/cmp.lua
@@ -0,0 +1,29 @@
+local cmp = require('cmp')
+
+local u = require('utils')
+local lspkind = require('lspkind')
+
+cmp.setup({
+    snippet = {expand = function(args) vim.fn["vsnip#anonymous"](args.body) end},
+    sources = {
+        {name = 'path'},
+        {name = 'buffer'},
+        {name = 'nvim_lsp'},
+        {name = 'nvim_lua'},
+    },
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({
+            -- TODO: I may infact what Insert here, sometimes a variable gets
+            -- replaced when completing and I dont' want that
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
+    },
+    formatting = {
+        format = function(entry, vim_item)
+            vim_item.kind = lspkind.presets.default[vim_item.kind]
+            return vim_item
+        end,
+    },
+})
+
