d382e88f3a6f7144309e70a39dda0e7220517272
Shawn Sorichetti <shawn@coloredblocks.com>
2021-09-09 10:45:17 -0400

    cleanup: Migrate efm to own file
    Future plans are to move away from this formatting to use null-ls, but
    first, separate out the efm configuration bits.

--- neovim/lua/lsp/init.lua
+++ neovim/lua/lsp/init.lua
@@ -1,4 +1,5 @@
 local lspconfig = require "lspconfig"
+local efm = require('lsp/efm')
 
 local map = vim.api.nvim_set_keymap
 
@@ -23,19 +24,6 @@ vim.fn.sign_define("LspDiagnosticsSignInformation",
 vim.fn.sign_define("LspDiagnosticsSignHint",
                    {text = "h", texthl = "LspDiagnosticsDefaultHint"})
 
-vim.lsp.handlers["textDocument/formatting"] =
-    function(err, _, result, _, bufnr)
-        if err ~= nil or result == nil then return end
-        if not vim.api.nvim_buf_get_option(bufnr, "modified") then
-            local view = vim.fn.winsaveview()
-            vim.lsp.util.apply_text_edits(result, bufnr)
-            vim.fn.winrestview(view)
-            if bufnr == vim.api.nvim_get_current_buf() then
-                vim.cmd [[noautocmd :update]]
-            end
-        end
-    end
-
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
     function(...)
         vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -45,36 +33,6 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
         })(...)
         pcall(vim.lsp.diagnostic.set_loclist, {open = false})
     end
-
-local format_options_prettier = {
-    tabWidth = 4,
-    singleQuote = true,
-    trailingComma = "all",
-    configPrecedence = "prefer-file",
-}
-vim.g.format_options_typescript = format_options_prettier
-vim.g.format_options_javascript = format_options_prettier
-vim.g.format_options_typescriptreact = format_options_prettier
-vim.g.format_options_javascriptreact = format_options_prettier
-vim.g.format_options_json = format_options_prettier
-vim.g.format_options_css = format_options_prettier
-vim.g.format_options_scss = format_options_prettier
-vim.g.format_options_html = format_options_prettier
-vim.g.format_options_yaml = format_options_prettier
-vim.g.format_options_markdown = format_options_prettier
-
-FormatToggle = function(value)
-    vim.g[string.format("format_disabled_%s", vim.bo.filetype)] = value
-end
-vim.cmd [[command! FormatDisable lua FormatToggle(true)]]
-vim.cmd [[command! FormatEnable lua FormatToggle(false)]]
-
-_G.formatting = function()
-    if not vim.g[string.format("format_disabled_%s", vim.bo.filetype)] then
-        vim.lsp.buf.formatting(vim.g[string.format("format_options_%s",
-                                                   vim.bo.filetype)] or {})
-    end
-end
 
 local on_attach = function(client)
     if client.resolved_capabilities.code_action then
@@ -238,45 +196,8 @@ lspconfig.terraformls.setup {
     filetypes = {"tf"},
 }
 
--- local vint = require "efm/vint"
-local luafmt = require "lsp/efm/luafmt"
-local black = require "lsp/efm/black"
-local isort = require "lsp/efm/isort"
-local flake8 = require "lsp/efm/flake8"
-local mypy = require "lsp/efm/mypy"
-local prettier = require "lsp/efm/prettier"
-local eslint = require "lsp/efm/eslint"
-local shellcheck = require "lsp/efm/shellcheck"
-local terraform = require "lsp/efm/terraform"
-local misspell = require "lsp/efm/misspell"
-
-local languages = {
-    ["="] = {misspell},
-    lua = {luafmt},
-    sh = {shellcheck},
-    -- vim = {vint},
-    -- python = {black, isort, flake8, mypy},
-    typescript = {prettier, eslint},
-    javascript = {prettier, eslint},
-    -- typescriptreact = {prettier, eslint},
-    -- javascriptreact = {prettier, eslint},
-    yaml = {prettier},
-    json = {prettier},
-    -- html = {prettier},
-    -- scss = {prettier},
-    -- css = {prettier},
-    markdown = {prettier},
-    -- tf = {terraform}
-}
-
--- https://github.com/mattn/efm-langserver
-lspconfig.efm.setup {
-    on_attach = on_attach,
-    init_options = {documentFormatting = true},
-    settings = {rootMarkers = {".git/", "go.mod"}, languages = languages},
-    filetypes = vim.tbl_keys(languages),
-}
-
 lspconfig.clangd.setup {on_attach = on_attach}
+
+efm.setup(on_attach, capabilities)
 
 require('lsp/lspsaga')
