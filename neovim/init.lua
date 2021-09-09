173971d23f9970ef06cac7fbe61a8498e572c8c6
Shawn Sorichetti <shawn@coloredblocks.com>
2021-09-09 09:44:52 -0400

    Migrate gitsigns to new layout

--- neovim/init.lua
+++ neovim/init.lua
@@ -82,17 +82,6 @@ require'telescope'.setup {
     },
 }
 
-require'gitsigns'.setup({
-    word_diff = true,
-    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-    current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
-    },
-    current_line_blame_formatter_opts = {relative_time = false},
-})
-
 require'nvim-autopairs'.setup()
 require("nvim-autopairs.completion.cmp").setup(
     {
