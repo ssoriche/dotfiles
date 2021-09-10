94741ca98a2d5974184191690073c3b10af0f381
Shawn Sorichetti <shawn@coloredblocks.com>
2021-09-09 21:36:22 -0400

    Apply lua formatting

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,37 +1,36 @@
-require("nvim-treesitter.configs").setup ({
-    ensure_installed = "all",
-    highlight = {enable = true},
-    indent = {enable = true},
-    refactor = {highlight_definitions = {enable = true}},
-    textobjects = {
-        select = {
+require("nvim-treesitter.configs").setup(
+    {
+        ensure_installed = "all",
+        highlight = {enable = true},
+        indent = {enable = true},
+        refactor = {highlight_definitions = {enable = true}},
+        textobjects = {
+            select = {
+                enable = true,
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                },
+            },
+            -- lsp_interop = {
+            --     enable = false,
+            --     peek_definition_code = {
+            --         ["df"] = "@function.outer",
+            --         ["dF"] = "@class.outer"
+            --     }
+            -- }
+        },
+        -- plugins
+        autopairs = {enable = true},
+        context_commentstring = {enable = true},
+        textsubjects = {
             enable = true,
             keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
+                ["."] = "textsubjects-smart",
+                [";"] = "textsubjects-container-outer",
             },
         },
-        -- lsp_interop = {
-        --     enable = false,
-        --     peek_definition_code = {
-        --         ["df"] = "@function.outer",
-        --         ["dF"] = "@class.outer"
-        --     }
-        -- }
-    },
-    -- plugins
-    autopairs = { enable = true },
-    context_commentstring = {enable = true},
-    textsubjects = {
-      enable = true,
-      keymaps = {
-        ["."] = "textsubjects-smart",
-        [";"] = "textsubjects-container-outer",
-      },
-    },
-    autotag = {
-        enable = true,
-    },
-})
+        autotag = {enable = true},
+    })
