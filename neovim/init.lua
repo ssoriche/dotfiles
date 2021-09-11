68c6c2b5fabf5d08d6ab8612fbede878e5d3d581
Shawn Sorichetti <shawn@coloredblocks.com>
2021-09-11 11:10:23 -0400

    Migrate telescope to new layout

--- neovim/init.lua
+++ neovim/init.lua
@@ -41,30 +41,5 @@ require('kommentary.config').configure_language('typescriptreact', {
     end,
 })
 
-local actions = require('telescope.actions')
-require'telescope'.setup {
-    config = {
-        layout_strategy = 'flex',
-        scroll_strategy = 'cycle',
-        mappings = {i = {["<esc>"] = actions.close}},
-        winblend = 0,
-        layout_defaults = {
-            horizontal = {
-                width_padding = 0.1,
-                height_padding = 0.1,
-                preview_width = 0.6,
-                -- mirror = false,
-            },
-            vertical = {
-                width_padding = 0.05,
-                height_padding = 1,
-                preview_height = 0.5,
-                -- mirror = true,
-            },
-        },
-        file_ignore_patterns = {'tags'},
-    },
-}
-
 require'unimpaired'.setup()
 require('tabout').setup()
