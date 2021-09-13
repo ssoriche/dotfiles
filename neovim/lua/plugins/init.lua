278646d866aec004e56c19b1baf9870bc7236eb1
Shawn Sorichetti <shawn@coloredblocks.com>
2021-09-12 20:40:13 -0400

    style: Stylua formatting

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -1,37 +1,37 @@
 -- Only required if you have packer in your `opt` pack
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function()
-    use {'wbthomason/packer.nvim', opt = true}
+return require("packer").startup(function()
+    use({ "wbthomason/packer.nvim", opt = true })
 
     local config = function(name)
         return string.format("require('plugins.%s')", name)
     end
 
     local use_with_config = function(path, name)
-        use({path, config = config(name)})
+        use({ path, config = config(name) })
     end
 
     use({
-        'hrsh7th/nvim-cmp',
+        "hrsh7th/nvim-cmp",
         requires = {
-            'hrsh7th/vim-vsnip',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-nvim-lua',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-emoji',
+            "hrsh7th/vim-vsnip",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-emoji",
         },
         config = config("cmp"),
     })
 
     -- treesitter
     use({
-        'nvim-treesitter/nvim-treesitter',
+        "nvim-treesitter/nvim-treesitter",
         run = ":TSUpdate",
         config = config("treesitter"),
     })
-    use {'RRethy/nvim-treesitter-textsubjects'}
+    use({ "RRethy/nvim-treesitter-textsubjects" })
 
     -- lsp
     use("nvim-lua/lsp-status.nvim")
@@ -39,101 +39,98 @@ return require('packer').startup(function()
     use({ "alexaandru/nvim-lspupdate" })
     use({ "jose-elias-alvarez/null-ls.nvim" })
 
-    use 'onsails/lspkind-nvim'
+    use("onsails/lspkind-nvim")
 
     use({
-        'lewis6991/gitsigns.nvim',
+        "lewis6991/gitsigns.nvim",
         config = config("git"),
-        requires = {'nvim-lua/plenary.nvim'},
+        requires = { "nvim-lua/plenary.nvim" },
     })
-    use {'machakann/vim-sandwich'}
+    use({ "machakann/vim-sandwich" })
 
-    use {'b3nj5m1n/kommentary'}
+    use({ "b3nj5m1n/kommentary" })
 
     use({
-        'windwp/nvim-autopairs',
+        "windwp/nvim-autopairs",
         config = config("autopairs"),
         wants = "nvim-cmp",
     })
 
-    use {'tpope/vim-eunuch'}
-    use {'tpope/vim-obsession'}
+    use({ "tpope/vim-eunuch" })
+    use({ "tpope/vim-obsession" })
 
     -- Themes
     use({
-        'nietiger/halcyon-neovim',
-        'folke/tokyonight.nvim',
-        'marko-cerovac/material.nvim',
-        'tjdevries/colorbuddy.nvim',
+        "nietiger/halcyon-neovim",
+        "folke/tokyonight.nvim",
+        "marko-cerovac/material.nvim",
+        "tjdevries/colorbuddy.nvim",
     })
 
-    use '9mm/vim-closer'
+    -- remove this
+    use("9mm/vim-closer")
 
     use({
-        'hoob3rt/lualine.nvim',
+        "hoob3rt/lualine.nvim",
         config = config("lualine"),
-        requires = {'kyazdani42/nvim-web-devicons', opt = true},
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
     })
 
     use_with_config("kyazdani42/nvim-web-devicons", "devicons")
 
     use({
-        'nvim-telescope/telescope.nvim',
-        config = config('telescope'),
-        requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
+        "nvim-telescope/telescope.nvim",
+        config = config("telescope"),
+        requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
     })
 
-    use 'tjdevries/astronauta.nvim'
-    use "lukas-reineke/indent-blankline.nvim"
+    use("tjdevries/astronauta.nvim")
+    use("lukas-reineke/indent-blankline.nvim")
 
-    use 'JoosepAlviste/nvim-ts-context-commentstring'
+    use("JoosepAlviste/nvim-ts-context-commentstring")
 
-    use {
+    use({
         "folke/which-key.nvim",
         config = function()
-            require("which-key").setup {
-                plugins = {spelling = {enabled = true, suggestions = 20}},
-            }
+            require("which-key").setup({
+                plugins = { spelling = { enabled = true, suggestions = 20 } },
+            })
         end,
-    }
+    })
 
-    use {
+    use({
         "folke/trouble.nvim",
         requires = "kyazdani42/nvim-web-devicons",
         config = function()
-            require("trouble").setup {
+            require("trouble").setup({
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
-            }
+            })
         end,
-    }
+    })
 
-    use {
+    use({
         "folke/todo-comments.nvim",
         config = function()
-            require("todo-comments").setup {
+            require("todo-comments").setup({
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
-            }
+            })
         end,
-    }
+    })
 
-    use {
+    use({
         "folke/lua-dev.nvim",
         config = function()
-            require("lua-dev").setup {
+            require("lua-dev").setup({
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
-            }
+            })
         end,
-    }
+    })
 
-    use {
-        'abecodes/tabout.nvim',
-        wants = {'nvim-treesitter'},
-    }
-
+    use({ "abecodes/tabout.nvim", wants = { "nvim-treesitter" } })
 end)
