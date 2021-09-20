02313da4d12c980fcd64d8933620541f158dd4ab
Shawn Sorichetti <shawn@coloredblocks.com>
2021-09-20 11:22:08 -0400

    style: Apply stylua formatting to treesitter

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,37 +1,36 @@
-require("nvim-treesitter.configs").setup(
-    {
-        ensure_installed = "all",
-        ignore_install = { "haskell" },
-        highlight = {enable = true},
-        indent = {enable = true},
-        refactor = {highlight_definitions = {enable = true}},
-        textobjects = {
-            select = {
-                enable = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                },
-            },
-            -- lsp_interop = {
-            --     enable = false,
-            --     peek_definition_code = {
-            --         ["df"] = "@function.outer",
-            --         ["dF"] = "@class.outer"
-            --     }
-            -- }
-        },
-        -- plugins
-        autopairs = {enable = true},
-        context_commentstring = {enable = true},
-        textsubjects = {
+require("nvim-treesitter.configs").setup({
+    ensure_installed = "all",
+    ignore_install = { "haskell" },
+    highlight = { enable = true },
+    indent = { enable = true },
+    refactor = { highlight_definitions = { enable = true } },
+    textobjects = {
+        select = {
             enable = true,
             keymaps = {
-                ["."] = "textsubjects-smart",
-                [";"] = "textsubjects-container-outer",
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
             },
         },
-        autotag = {enable = true},
-    })
+        -- lsp_interop = {
+        --     enable = false,
+        --     peek_definition_code = {
+        --         ["df"] = "@function.outer",
+        --         ["dF"] = "@class.outer"
+        --     }
+        -- }
+    },
+    -- plugins
+    autopairs = { enable = true },
+    context_commentstring = { enable = true },
+    textsubjects = {
+        enable = true,
+        keymaps = {
+            ["."] = "textsubjects-smart",
+            [";"] = "textsubjects-container-outer",
+        },
+    },
+    autotag = { enable = true },
+})
