db468b53d7a5e3f8a2faa4cc0c1728f6c417b3e6
Shawn Sorichetti <shawn@coloredblocks.com>
2021-09-20 11:20:13 -0400

    fix: Add ignore for haskell in treesitter
    I currently do not write any haskell code and the treesitter
    installation always seems to fail throwing a large quantity of error
    messages that are really annoying to deal with. Just going to ignore it
    for now at least.

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 require("nvim-treesitter.configs").setup(
     {
         ensure_installed = "all",
+        ignore_install = { "haskell" },
         highlight = {enable = true},
         indent = {enable = true},
         refactor = {highlight_definitions = {enable = true}},
