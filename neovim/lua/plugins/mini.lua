a411c5e8739d853aef6ad1c6caf62a3b70769435
Shawn Sorichetti <shawn@coloredblocks.com>
2021-11-22 20:15:55 -0500

    feat!: remap surround keys to use g prefix
    In order to try out ggandor/lightspeed.nvim need to move the `s`
    keymapping to something different. Prefixing with the `g` key
    combination seems to make sense. In the past with vim-surround `y` was
    used, but `g` still seems more logical.

--- neovim/lua/plugins/mini.lua
+++ neovim/lua/plugins/mini.lua
@@ -1,1 +1,11 @@
-require("mini.surround").setup()
+require("mini.surround").setup({
+    mappings = {
+        add = "gsa", -- Add surrounding
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+    },
+})
