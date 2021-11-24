b7849b0110909eaf1f97bc1838e0fa15d1cb3dd5
Shawn Sorichetti <shawn@coloredblocks.com>
2021-11-24 11:46:36 -0500

    feat: Add incremental select via treesitter
    Found this in the subreddit earlier, increasing the selected text based
    on treesitter automatically.

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -33,4 +33,13 @@ require("nvim-treesitter.configs").setup({
         },
     },
     autotag = { enable = true },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<CR>",
+            scope_incremental = "<CR>",
+            node_incremental = "<TAB>",
+            node_decremental = "<S-TAB>",
+        },
+    },
 })
