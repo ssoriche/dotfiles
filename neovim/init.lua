0f3bc6c5c2b0da401f19ab13e20a43ed00238caa
Shawn Sorichetti <shawn@coloredblocks.com>
2021-11-26 19:09:55 -0500

    style:  formatted with stylua

--- neovim/init.lua
+++ neovim/init.lua
@@ -3,22 +3,22 @@ local fn = vim.fn -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g -- a table to access global variables
 local execute = vim.api.nvim_command
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-    execute('!git clone https://github.com/wbthomason/packer.nvim ' ..
-                install_path)
-    execute 'packadd packer.nvim'
+    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+    execute("packadd packer.nvim")
 end
 
 _G.global = {}
+_G.global.yaml = {}
 
 g.indent_blankline_char = "│"
 
-require('plugins')
-require('lsp')
-require('settings')
-require('maps')
+require("plugins")
+require("lsp")
+require("settings")
+require("maps")
 
 --[[ g.material_style = 'deep ocean'
 g.material_italics = 1
@@ -26,10 +26,10 @@ require('material').set() ]]
 
 g.tokyonight_style = "night"
 g.tokyonight_transparent = true
-vim.cmd [[colorscheme tokyonight]]
+vim.cmd([[colorscheme tokyonight]])
 
 --[[ local ts = require 'nvim-treesitter.configs'
 ts.setup {ensure_installed = 'maintained', highlight = {enable = true}} ]]
 
-require'unimpaired'.setup()
-require('tabout').setup()
+require("unimpaired").setup()
+require("tabout").setup()
