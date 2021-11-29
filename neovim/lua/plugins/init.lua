ec149b92e91cf4f37d1207dd2d84d0988f0bd415
Shawn Sorichetti <shawn@coloredblocks.com>
2021-11-29 09:30:24 -0500

    Revert "feat!: Remove ggandor/lightspeed.nvim"
    This reverts commit a665049c0159bbd3042c4defe57d4ae664c120c3.

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -66,6 +66,8 @@ return require("packer").startup(function()
     use({ "goolord/alpha-nvim", config = config("dashboard") })
     use({ "folke/persistence.nvim", config = config("session") })
 
+    use({ "ggandor/lightspeed.nvim" })
+
     -- Themes
     use({
         "nietiger/halcyon-neovim",
