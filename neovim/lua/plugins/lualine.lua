6a7e78cea26bbb4be92dcf355433a696c53ef59d
Shawn Sorichetti <shawn@coloredblocks.com>
2021-12-28 09:37:38 -0500

    style: formatted with stylus

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -1,22 +1,22 @@
-local lualine = require('lualine')
+local lualine = require("lualine")
 
-lualine.setup {
+lualine.setup({
     -- options = {theme = 'material-nvim'},
     sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch'},
-        lualine_c = {{'filename', path = 1}},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'},
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
     },
     inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
         lualine_y = {},
         lualine_z = {},
     },
-    extensions = {'quickfix'},
-}
+    extensions = { "quickfix" },
+})
