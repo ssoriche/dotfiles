f35c5f0ca9a5bb13aa5e8af0f4e575c9d49b77f3
Shawn Sorichetti <shawn@coloredblocks.com>
2022-04-07 20:25:09 -0400

    feat(neovim): Move lua-language-server
    Instead of keeping this in the git checkout, move to ~/bin

--- neovim/lua/lsp/sumneko.lua
+++ neovim/lua/lsp/sumneko.lua
@@ -4,12 +4,15 @@ local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-local root = vim.fn.getenv("HOME") .. "/git/lua-language-server/"
-local binary = root .. "bin/macOS/lua-language-server"
+local root = vim.fn.getenv("HOME") .. "/bin/lua-language-server/"
+local binary = root .. "/bin/lua-language-server"
 local settings = {
     Lua = {
-        runtime = {version = "LuaJIT", path = runtime_path},
-        workspace = {library = vim.api.nvim_get_runtime_file("", true)},
+        runtime = { version = "LuaJIT", path = runtime_path },
+        workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false,
+        },
         diagnostics = {
             enable = true,
             globals = {
@@ -22,19 +25,21 @@ local settings = {
                 "after_each",
             },
         },
+        telemetry = {
+            enable = false,
+        },
     },
 }
 
 local M = {}
 M.setup = function(on_attach, capabilities)
-    local luadev = require"lua-dev".setup(
-                       {lspconfig = {cmd = {"lua-language-server"}}})
+    local luadev = require("lua-dev").setup({ lspconfig = { cmd = { "lua-language-server" } } })
 
     lspconfig.sumneko_lua.setup({
         on_attach = on_attach,
-        cmd = {binary, "-E", root .. "main.lua"},
+        cmd = { binary, "-E", root .. "main.lua" },
         settings = settings,
-        flags = {debounce_text_changes = 150},
+        flags = { debounce_text_changes = 150 },
         capabilities = capabilities,
     })
 end
