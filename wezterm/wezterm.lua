c1f389c834dc0ec46cabc477d49e9c819d305d5e
Shawn Sorichetti <shawn@coloredblocks.com>
2022-06-15 14:18:04 -0400

    feat(term): increase font size
    While using the display of my work laptop, became used to the larger
    font size when sharing screens with people. Increasing it everywhere.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -30,7 +30,7 @@ wezterm.on("toggle-leader", function(window, pane)
 end)
 
 return {
-	font_size = 12.0,
+	font_size = 14.0,
 	font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font", "JuliaMono" }),
 	allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
 	color_scheme = "wezterm_kanagawa",
