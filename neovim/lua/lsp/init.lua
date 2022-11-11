8468bbb3b139318542007aaacc40cb9a45bfd265
Shawn Sorichetti <shawn@coloredblocks.com>
2022-11-11 16:44:09 -0500

    chore(neovim): fix cmp call for capabilities
    The method update_capabilities is deprecated in favour of
    default_capabilities moving to that to squash the deprecation notices.

--- neovim/lua/lsp/init.lua
+++ neovim/lua/lsp/init.lua
@@ -28,7 +28,7 @@ _G.global.lsp = { popup_opts = popup_opts }
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local on_attach = function(client, bufnr)
     bufnr = bufnr or 0
