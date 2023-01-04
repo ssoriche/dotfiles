0415e6cfaf8436319cf25286d48212a0973577f3
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2023-01-04 13:55:25 -0500

    fix(nvim): use https for lazy plugin
    The use of ssh always prompts for a passcode to unlock access to my ssh
    keys to clone using the git url. Changing to https so as not to require
    authentication.

--- neovim/lua/config/lazy.lua
+++ neovim/lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "--single-branch",
-    "git@github.com:folke/lazy.nvim.git",
+    "https://github.com/folke/lazy.nvim.git",
     lazypath,
   })
 end
