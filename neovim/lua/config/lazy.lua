357c7af46bb02135fba1d33fcd08e6e6e7b8f6dc
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2023-01-12 17:53:23 -0500

    chore(nvim): Change lazy.nvim bootstrap git commands
    Follow the stable tag.

--- neovim/lua/config/lazy.lua
+++ neovim/lua/config/lazy.lua
@@ -1,14 +1,9 @@
 -- bootstrap from github
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
+  vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" }) -- last stable release
+  vim.opt.rtp:prepend(lazypath)
 end
 vim.opt.runtimepath:prepend(lazypath)
 
