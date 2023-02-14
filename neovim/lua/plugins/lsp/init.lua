af747d79317fd25dba44253acf8993ddde7fdfa3
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2023-02-14 16:56:18 -0500

    chore(neovim): replace sumneko_lua with lua_ls
    Deprecated in newer versions of lsp config, replace the use of
    sumneko_lua with lua_ls as per LazyVim project.

--- neovim/lua/plugins/lsp/init.lua
+++ neovim/lua/plugins/lsp/init.lua
@@ -67,52 +67,14 @@ function M.config()
       },
     },
     yamlls = {},
-    sumneko_lua = {
-      -- cmd = { "/home/folke/projects/lua-language-server/bin/lua-language-server" },
-      single_file_support = true,
+    lua_ls = {
       settings = {
         Lua = {
           workspace = {
             checkThirdParty = false,
           },
           completion = {
-            workspaceWord = true,
-            callSnippet = "Both",
-          },
-          misc = {
-            parameters = {
-              "--log-level=trace",
-            },
-          },
-          diagnostics = {
-            -- enable = false,
-            groupSeverity = {
-              strong = "Warning",
-              strict = "Warning",
-            },
-            groupFileStatus = {
-              ["ambiguity"] = "Opened",
-              ["await"] = "Opened",
-              ["codestyle"] = "None",
-              ["duplicate"] = "Opened",
-              ["global"] = "Opened",
-              ["luadoc"] = "Opened",
-              ["redefined"] = "Opened",
-              ["strict"] = "Opened",
-              ["strong"] = "Opened",
-              ["type-check"] = "Opened",
-              ["unbalanced"] = "Opened",
-              ["unused"] = "Opened",
-            },
-            unusedLocalExclude = { "_*" },
-          },
-          format = {
-            enable = true,
-            defaultConfig = {
-              indent_style = "space",
-              indent_size = "2",
-              continuation_indent_size = "2",
-            },
+            callSnippet = "Replace",
           },
         },
       },
