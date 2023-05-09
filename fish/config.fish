0d33cfcc84b496f0eb796c5f8707e315d0d01b3c
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2023-05-09 10:05:59 -0400

    feat(shell): add support for atuin
    If installed, add atuin for command line history stored in a sqlite
    database.

--- fish/config.fish
+++ fish/config.fish
@@ -79,6 +79,10 @@ if command -v anyenv >/dev/null
     source (anyenv init - fish|psub)
 end
 
+if command -v atuin >/dev/null
+    atuin init fish | source
+end
+
 set -Ux FZF_DEFAULT_OPTS "--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
 if test -e /usr/local/opt/asdf/asdf.fish
     source /usr/local/opt/asdf/asdf.fish
