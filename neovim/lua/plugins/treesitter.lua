d3a7c858c1cd5be5a992d50b215689d64851f732
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2023-05-18 17:11:53 -0400

    feat(neovim): add gotmpl support to treesitter
    Doing some work within gotmpl's and want my editor to understand what
    I'm working with.

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -144,6 +144,14 @@ return {
       })
       local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
       parser_config.markdown.filetype_to_parsername = "octo"
+      parser_config.gotmpl = {
+        install_info = {
+          url = "https://github.com/ngalaiko/tree-sitter-go-template",
+          files = { "src/parser.c" },
+        },
+        filetype = "gotmpl",
+        used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl" },
+      }
     end,
   },
 }
