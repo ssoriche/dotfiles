b4d195afda1c9943d5482c31bde7bcf5f965bbc6
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2023-05-19 18:04:46 -0400

    futzing

--- neovim/lua/plugins/lsp/init.lua
+++ neovim/lua/plugins/lsp/init.lua
@@ -77,32 +77,33 @@ return {
     config = function(_, opts)
       local Util = require("lazyvim.util")
       -- setup autoformat
-      require("lazyvim.plugins.lsp.format").autoformat = opts.autoformat
+      -- require("lazyvim.plugins.lsp.format").autoformat = opts.autoformat
       -- setup formatting and keymaps
       Util.on_attach(function(client, buffer)
-        require("lazyvim.plugins.lsp.format").on_attach(client, buffer)
-        require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
+        require("plugins.lsp.formatting").setup()
+        require("plugins.lsp.keymaps").on_attach(client, buffer)
       end)
 
       -- diagnostics
-      for name, icon in pairs(require("lazyvim.config").icons.diagnostics) do
-        name = "DiagnosticSign" .. name
-        vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
-      end
-
-      if type(opts.diagnostics.virtual_text) == "table" and opts.diagnostics.virtual_text.prefix == "icons" then
-        opts.diagnostics.virtual_text.prefix = vim.fn.has("nvim-0.10.0") == 0 and "●"
-          or function(diagnostic)
-            local icons = require("lazyvim.config").icons.diagnostics
-            for d, icon in pairs(icons) do
-              if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
-                return icon
-              end
-            end
-          end
-      end
-
-      vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+      require("plugins.lsp.diagnostics").setup()
+      -- for name, icon in pairs(require("lazyvim.config").icons.diagnostics) do
+      --   name = "DiagnosticSign" .. name
+      --   vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+      -- end
+      --
+      -- if type(opts.diagnostics.virtual_text) == "table" and opts.diagnostics.virtual_text.prefix == "icons" then
+      --   opts.diagnostics.virtual_text.prefix = vim.fn.has("nvim-0.10.0") == 0 and "●"
+      --     or function(diagnostic)
+      --       local icons = require("lazyvim.config").icons.diagnostics
+      --       for d, icon in pairs(icons) do
+      --         if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+      --           return icon
+      --         end
+      --       end
+      --     end
+      -- end
+      --
+      -- vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
 
       local servers = opts.servers
       local capabilities = vim.tbl_deep_extend(
