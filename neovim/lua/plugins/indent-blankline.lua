41146889ac7933039e31dfb4260fd2aa3aded61a
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2023-11-14 15:39:55 -0500

    chore(neovim): fix indent-blankline configuration
    With a version change to indent-blankline the configuration contents
    have changed.

--- neovim/lua/plugins/indent-blankline.lua
+++ neovim/lua/plugins/indent-blankline.lua
@@ -1,45 +1,11 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
   event = "BufReadPre",
-  opts = {
-    buftype_exclude = { "terminal", "nofile" },
-    filetype_exclude = {
-      "help",
-      "startify",
-      "dashboard",
-      "packer",
-      "neogitstatus",
-      "NvimTree",
-      "neo-tree",
-      "Trouble",
-    },
-    char = "▏",
-    use_treesitter_scope = false,
-    show_trailing_blankline_indent = false,
-    show_current_context = true,
-    context_patterns = {
-      "class",
-      "return",
-      "function",
-      "method",
-      "^if",
-      "^while",
-      "jsx_element",
-      "^for",
-      "^object",
-      "^table",
-      "block",
-      "arguments",
-      "if_statement",
-      "else_clause",
-      "jsx_element",
-      "jsx_self_closing_element",
-      "try_statement",
-      "catch_clause",
-      "import_statement",
-      "operation_type",
-    },
-  },
+  main = "ibl",
 }
+
+function M.config()
+  require("ibl").setup()
+end
 
 return M
