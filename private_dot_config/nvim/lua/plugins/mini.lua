622ad6eac85cc2132bb788041d41e989b04d5abb
Shawn Sorichetti <ssoriche@OptimusPrime.cobalt.coloredblocks.net>
2023-11-20 21:59:26 -0500

    refactor(neovim): update mini configuration to LazyVim style
    Using the LazyVim configuration style, update the mini configuration
    with options that I use.

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -1,97 +1,177 @@
-local mini = {
-  "echasnovski/mini.nvim",
-  event = "VeryLazy",
-}
-
-local specs = { mini, "JoosepAlviste/nvim-ts-context-commentstring" }
-
-function mini.surround()
-  require("mini.surround").setup({
-    mappings = {
-      add = "gza", -- Add surrounding in Normal and Visual modes
-      delete = "gzd", -- Delete surrounding
-      find = "gzf", -- Find surrounding (to the right)
-      find_left = "gzF", -- Find surrounding (to the left)
-      highlight = "gzh", -- Highlight surrounding
-      replace = "gzr", -- Replace surrounding
-      update_n_lines = "gzn", -- Update `n_lines`
+return {
+  -- auto pairs
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "<leader>up",
+        function()
+          local Util = require("lazy.core.util")
+          vim.g.minipairs_disable = not vim.g.minipairs_disable
+          if vim.g.minipairs_disable then
+            Util.warn("Disabled auto pairs", { title = "Option" })
+          else
+            Util.info("Enabled auto pairs", { title = "Option" })
+          end
+        end,
+        desc = "Toggle auto pairs",
+      },
     },
-  })
-end
-
-function mini.jump()
-  require("mini.jump").setup({})
-end
-
-function mini.pairs()
-  require("mini.pairs").setup({})
-end
-
-function mini.comment()
-  require("mini.comment").setup({
-    hooks = {
-      pre = function()
-        require("ts_context_commentstring.internal").update_commentstring({})
-      end,
+  },
+  {
+    "echasnovski/mini.surround",
+    keys = function(_, keys)
+      -- Populate the keys based on the user's options
+      local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+      local mappings = {
+        { opts.mappings.add,            desc = "Add surrounding",                     mode = { "n", "v" } },
+        { opts.mappings.delete,         desc = "Delete surrounding" },
+        { opts.mappings.find,           desc = "Find right surrounding" },
+        { opts.mappings.find_left,      desc = "Find left surrounding" },
+        { opts.mappings.highlight,      desc = "Highlight surrounding" },
+        { opts.mappings.replace,        desc = "Replace surrounding" },
+        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+      }
+      mappings = vim.tbl_filter(function(m)
+        return m[1] and #m[1] > 0
+      end, mappings)
+      return vim.list_extend(mappings, keys)
+    end,
+    opts = {
+      mappings = {
+        add = "gza",            -- Add surrounding in Normal and Visual modes
+        delete = "gzd",         -- Delete surrounding
+        find = "gzf",           -- Find surrounding (to the right)
+        find_left = "gzF",      -- Find surrounding (to the left)
+        highlight = "gzh",      -- Highlight surrounding
+        replace = "gzr",        -- Replace surrounding
+        update_n_lines = "gzn", -- Update `n_lines`
+      },
     },
-  })
-end
-
-function mini.bracketed()
-  require("mini.bracketed").setup({})
-end
-
-function mini.ai()
-  local ai = require("mini.ai")
-  require("mini.ai").setup({
-    n_lines = 500,
-    -- search_method = "cover_or_next",
-    custom_textobjects = {
-      o = ai.gen_spec.treesitter({
-        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-        i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-      }, {}),
-      f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-      c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+  },
+  -- comments
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    opts = {
+      enable_autocmd = false,
     },
-  })
-
-  local map = function(text_obj, desc)
-    for _, side in ipairs({ "left", "right" }) do
-      for dir, d in pairs({ prev = "[", next = "]" }) do
-        local lhs = d .. (side == "right" and text_obj:upper() or text_obj:lower())
-        for _, mode in ipairs({ "n", "x", "o" }) do
-          vim.keymap.set(mode, lhs, function()
-            ai.move_cursor(side, "a", text_obj, { search_method = dir })
-          end, {
-            desc = dir .. " " .. desc,
-          })
+  },
+  {
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        custom_commentstring = function()
+          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    },
+  },
+  {
+    'echasnovski/mini.bracketed',
+    opts = {},
+    event = "VeryLazy",
+    version = false
+  },
+  -- Better text-objects
+  {
+    "echasnovski/mini.ai",
+    -- keys = {
+    --   { "a", mode = { "x", "o" } },
+    --   { "i", mode = { "x", "o" } },
+    -- },
+    event = "VeryLazy",
+    opts = function()
+      local ai = require("mini.ai")
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }, {}),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("mini.ai").setup(opts)
+      -- register all text objects with which-key
+      require("util").on_load("which-key.nvim", function()
+        ---@type table<string, string|table>
+        local i = {
+          [" "] = "Whitespace",
+          ['"'] = 'Balanced "',
+          ["'"] = "Balanced '",
+          ["`"] = "Balanced `",
+          ["("] = "Balanced (",
+          [")"] = "Balanced ) including white-space",
+          [">"] = "Balanced > including white-space",
+          ["<lt>"] = "Balanced <",
+          ["]"] = "Balanced ] including white-space",
+          ["["] = "Balanced [",
+          ["}"] = "Balanced } including white-space",
+          ["{"] = "Balanced {",
+          ["?"] = "User Prompt",
+          _ = "Underscore",
+          a = "Argument",
+          b = "Balanced ), ], }",
+          c = "Class",
+          f = "Function",
+          o = "Block, conditional, loop",
+          q = "Quote `, \", '",
+          t = "Tag",
+        }
+        local a = vim.deepcopy(i)
+        for k, v in pairs(a) do
+          a[k] = v:gsub(" including.*", "")
         end
-      end
-    end
-  end
 
-  map("f", "function")
-  map("c", "class")
-  map("o", "block")
-end
+        local ic = vim.deepcopy(i)
+        local ac = vim.deepcopy(a)
+        for key, name in pairs({ n = "Next", l = "Last" }) do
+          i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
+          a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
+        end
+        require("which-key").register({
+          mode = { "o", "x" },
+          i = i,
+          a = a,
+        })
+      end)
+    end,
+  },
+  -- buffer remove
+  {
+    "echasnovski/mini.bufremove",
 
-function mini.config()
-  -- M.jump()
-  mini.surround()
-  mini.ai()
-  mini.pairs()
-  mini.comment()
-  mini.bracketed()
-end
-
-function mini.init()
-  vim.keymap.set("n", "<leader>bd", function()
-    require("mini.bufremove").delete(0, false)
-  end)
-  vim.keymap.set("n", "<leader>bD", function()
-    require("mini.bufremove").delete(0, true)
-  end)
-end
-
-return specs
+    keys = {
+      {
+        "<leader>bd",
+        function()
+          local bd = require("mini.bufremove").delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+            if choice == 1 then -- Yes
+              vim.cmd.write()
+              bd(0)
+            elseif choice == 2 then -- No
+              bd(0, true)
+            end
+          else
+            bd(0)
+          end
+        end,
+        desc = "Delete Buffer",
+      },
+      -- stylua: ignore
+      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+    },
+  },
+}
