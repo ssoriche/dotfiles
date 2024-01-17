4471bb933f4af78be3d4a324b2fcd1a3d2f8f063
Shawn Sorichetti <shawn@coloredblocks.com>
2024-01-16 21:44:04 -0500

    feat(nvim)!: removed treesj plugin
    This plugin interferes with the natural use of J to join lines in files
    like gitcommit or markdown. Files that this plugin isn't designed for
    use in yet I edit often. Removing until a solution is found to use this
    plugin with a reliable fallback.

--- private_dot_config/nvim/lua/plugins/coding.lua
+++ private_dot_config/nvim/lua/plugins/coding.lua
@@ -65,13 +65,13 @@ return {
     end,
   },
 
-  {
-    "Wansmer/treesj",
-    keys = {
-      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
-    },
-    opts = { use_default_keymaps = false, max_join_length = 150 },
-  },
+  -- {
+  --   "Wansmer/treesj",
+  --   keys = {
+  --     { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+  --   },
+  --   opts = { use_default_keymaps = false, max_join_length = 150 },
+  -- },
 
   {
     "cshuaimin/ssr.nvim",
