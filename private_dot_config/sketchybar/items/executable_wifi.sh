ad1d85569fecd99246ee6dd6ad5cc16cbc1ae315
Shawn Sorichetti <shawns@ziprecruiter.com>
2024-03-28 17:37:13 -0400

    feat(sketchybar)!: remove sketchybar configuration
    Temporarily remove the configuration for sketchybar as I'm migrating to
    a new configuration based in lua.

--- private_dot_config/sketchybar/items/executable_wifi.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-#
-sketchybar --add item wifi right \
-  --set wifi script="$PLUGIN_DIR/wifi.sh" \
-  background.padding_right=12 \
-  update_freq=5
--- private_dot_config/sketchybar/plugins/executable_disk.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#disk.sh
-
-sketchybar -m --set "$NAME" label="$(df -H | grep -E '^(/dev/disk3s5).' | awk '{ printf ("%s\n", $5) }')"
