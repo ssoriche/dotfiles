046d6086bd993212cd484f5cb32b2f27669e57c6
Shawn Sorichetti <shawns@ziprecruiter.com>
2024-04-09 15:56:03 -0400

    fix(neovim): 'unmap' cursor keys
    My cursor key bindings are ctrl-h,j,k,l and LazyVim has mappings for
    these keys, map the cursor keys to the key mappings that LazyVim sets
    up.

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Disable cursor keys, I use ctrl-j,k,l,h as cursor keys
+-- these mappings set the cursor keys to LazyVim's ctrl-j,k,l,h mappings
+vim.keymap.set("n", "<Down>", "<C-w>j")
+vim.keymap.set("n", "<Up>", "<C-w>k")
+vim.keymap.set("n", "<Right>", "<C-w>l")
+vim.keymap.set("n", "<Left>", "<C-w>h")
