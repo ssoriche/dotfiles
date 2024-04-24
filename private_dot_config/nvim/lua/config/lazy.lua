7c9b0de59b625c3f408136cc0dc83ed5a41b3823
Shawn Sorichetti <shawns@ziprecruiter.com>
2024-04-24 16:44:13 -0700

    feat(neovim): enable copilot
    Work is paying for copilot, let's see what it does.

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
