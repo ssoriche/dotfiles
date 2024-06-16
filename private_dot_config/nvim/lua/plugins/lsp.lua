aaf266ea0e6dcfffb3c77e622ac88a20b60c6790
Shawn Sorichetti <shawn@coloredblocks.com>
2024-06-16 10:33:47 -0400

    conf(neovim): Clean up lsp configuration
    Lots of changes have happened to lsp configuration removing a lot of
    what was manually configured before.

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -18,23 +18,9 @@ return {
   -- lsp servers
   {
     "neovim/nvim-lspconfig",
-    dependences = {
-      { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-      {
-        "folke/neodev.nvim",
-        opts = {
-          debug = true,
-          experimental = {
-            pathStrict = true,
-          },
-          -- library = {
-          --   runtime = "~/projects/neovim/runtime/",
-          -- },
-        },
-      },
-    },
     opts = {
-      inlay_hints = { enabled = true },
+      diagnostics = { virtual_text = { prefix = "icons" } },
+      inlay_hints = { enabled = false },
       capabilities = {
         workspace = {
           didChangeWatchedFiles = {
@@ -44,104 +30,15 @@ return {
       },
       ---@type lspconfig.options
       servers = {
-        ansiblels = {},
-        bashls = {},
-        clangd = {},
-        -- denols = {},
-        cssls = {},
-        dockerls = {},
-        -- ruff_lsp = {},
-        tailwindcss = {
-          root_dir = function(...)
-            return require("lspconfig.util").root_pattern(".git")(...)
-          end,
-        },
-        tsserver = {
-          -- root_dir = function(...)
-          --   return require("lspconfig.util").root_pattern(".git")(...)
-          -- end,
-          single_file_support = false,
-          settings = {
-            typescript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "literal",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = false,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-            javascript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-          },
-        },
-        -- svelte = {},
-        html = {},
-        -- gopls = {},
-        marksman = {},
-        -- pyright = {
-        --   enabled = false,
-        -- },
-        -- rust_analyzer = {
-        -- settings = {
-        --   ["rust-analyzer"] = {
-        --     procMacro = { enable = true },
-        --     cargo = { allFeatures = true },
-        --     checkOnSave = {
-        --       command = "clippy",
-        --       extraArgs = { "--no-deps" },
-        --     },
-        --   },
-        -- },
-        -- },
-        yamlls = {
-          settings = {
-            yaml = {
-              keyOrdering = false,
-            },
-          },
-        },
         lua_ls = {
-          -- enabled = false,
           -- cmd = { "/home/folke/projects/lua-language-server/bin/lua-language-server" },
-          single_file_support = true,
+          -- single_file_support = true,
           settings = {
             Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              completion = {
-                workspaceWord = true,
-                callSnippet = "Both",
-              },
               misc = {
-                parameters = {
-                  -- "--log-level=trace",
-                },
+                -- parameters = { "--loglevel=trace" },
               },
-              hover = { expandAlias = false },
-              hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
-              doc = {
-                privateName = { "^_" },
-              },
+              -- hover = { expandAlias = false },
               type = {
                 castNumberToInteger = true,
               },
@@ -168,27 +65,10 @@ return {
                 },
                 unusedLocalExclude = { "_*" },
               },
-              format = {
-                enable = true,
-                defaultConfig = {
-                  indent_style = "space",
-                  indent_size = "2",
-                  continuation_indent_size = "2",
-                },
-              },
             },
           },
         },
-        vimls = {},
       },
-      setup = {},
-    },
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      diagnostics = { virtual_text = { prefix = "icons" } },
     },
   },
 
@@ -197,19 +77,14 @@ return {
     optional = true,
     opts = {
       formatters_by_ft = {
-        ["markdown"] = { { "prettierd", "prettier" } },
-        ["markdown.mdx"] = { { "prettierd", "prettier" } },
-        ["javascript"] = { "dprint" },
+        ["javascript"] = { "dprint", { "prettierd", "prettier" } },
         ["javascriptreact"] = { "dprint" },
-        ["typescript"] = { "dprint" },
+        ["typescript"] = { "dprint", { "prettierd", "prettier" } },
         ["typescriptreact"] = { "dprint" },
       },
       formatters = {
-        shfmt = {
-          prepend_args = { "-i", "2", "-ci" },
-        },
         dprint = {
-          condition = function(ctx)
+          condition = function(_, ctx)
             return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
           end,
         },
@@ -221,18 +96,24 @@ return {
     opts = {
       linters_by_ft = {
         lua = { "selene", "luacheck" },
-        markdown = { "markdownlint", "write_good" },
-        gitcommit = { "write_good" },
       },
       linters = {
         selene = {
           condition = function(ctx)
-            return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
+            local root = LazyVim.root.get({ normalize = true })
+            if root ~= vim.uv.cwd() then
+              return false
+            end
+            return vim.fs.find({ "selene.toml" }, { path = root, upward = true })[1]
           end,
         },
         luacheck = {
           condition = function(ctx)
-            return vim.fs.find({ ".luacheckrc" }, { path = ctx.filename, upward = true })[1]
+            local root = LazyVim.root.get({ normalize = true })
+            if root ~= vim.uv.cwd() then
+              return false
+            end
+            return vim.fs.find({ ".luacheckrc" }, { path = root, upward = true })[1]
           end,
         },
       },
