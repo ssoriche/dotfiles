61225c2332f950e97270d52229f474968596afb6
Shawn Sorichetti <shawns@ziprecruiter.com>
2025-04-25 17:47:11 -0400

    style(hammerspoon)

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -5,8 +5,8 @@ hs.loadSpoon("SpoonInstall")
 hs.loadSpoon("EmmyLua")
 
 spoon.SpoonInstall.repos.zzspoons = {
-  url = "https://github.com/zzamboni/zzSpoons",
-  desc = "zzamboni's spoon repository",
+	url = "https://github.com/zzamboni/zzSpoons",
+	desc = "zzamboni's spoon repository",
 }
 
 local hyper = { "cmd", "alt", "ctrl" }
@@ -18,41 +18,41 @@ spoon.SpoonInstall.use_syncinstall = true
 Install = spoon.SpoonInstall
 
 Install:andUse("TextClipboardHistory", {
-  -- disable = true,
-  config = {
-    show_in_menubar = false,
-  },
-  hotkeys = {
-    toggle_clipboard = { { "cmd", "alt" }, "v" },
-  },
-  start = true,
+	-- disable = true,
+	config = {
+		show_in_menubar = false,
+	},
+	hotkeys = {
+		toggle_clipboard = { { "cmd", "alt" }, "v" },
+	},
+	start = true,
 })
 
 Install:andUse("Seal", {
-  -- hotkeys = { show = { {"cmd", "ctrl" }, "space" } },
-  hotkeys = { show = { { "cmd" }, "space" } },
-  fn = function(s)
-    s:loadPlugins({ "apps", "calc", "safari_bookmarks", "screencapture", "useractions", "urlformats" })
-    s.plugins.safari_bookmarks.always_open_with_safari = false
-    s.plugins.useractions.actions = {
-      ["Hammerspoon docs webpage"] = {
-        url = "https://hammerspoon.org/docs/",
-        icon = hs.image.imageFromName(hs.image.systemImageNames.ApplicationIcon),
-      },
-    }
-    s.plugins.urlformats:providersTable({
-      gh = { name = "GitHub", url = "https://github.com/%s" },
-      go = { name = "golinks", url = "https://go.metroplex.coloredblocks.net/%s" },
-    })
-    s:refreshAllCommands()
-  end,
-  start = true,
+	-- hotkeys = { show = { {"cmd", "ctrl" }, "space" } },
+	hotkeys = { show = { { "cmd" }, "space" } },
+	fn = function(s)
+		s:loadPlugins({ "apps", "calc", "safari_bookmarks", "screencapture", "useractions", "urlformats" })
+		s.plugins.safari_bookmarks.always_open_with_safari = false
+		s.plugins.useractions.actions = {
+			["Hammerspoon docs webpage"] = {
+				url = "https://hammerspoon.org/docs/",
+				icon = hs.image.imageFromName(hs.image.systemImageNames.ApplicationIcon),
+			},
+		}
+		s.plugins.urlformats:providersTable({
+			gh = { name = "GitHub", url = "https://github.com/%s" },
+			go = { name = "golinks", url = "https://go.metroplex.coloredblocks.net/%s" },
+		})
+		s:refreshAllCommands()
+	end,
+	start = true,
 })
 
 Install:andUse("Caffeine", {
-  start = true,
-  hotkeys = {
-    toggle = { hyper, "1" },
-  },
-  --                 fn = BTT_caffeine_widget,
+	start = true,
+	hotkeys = {
+		toggle = { hyper, "1" },
+	},
+	--                 fn = BTT_caffeine_widget,
 })
