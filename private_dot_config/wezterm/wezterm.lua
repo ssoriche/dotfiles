60e4c564ac1013ea6803c324c677a0bd56ef4869
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2025-10-17 17:12:15 -0400

    conf(wezterm): Allow terminal to send shift+enter
    Used to create new lines in ClaudeCode.

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -108,5 +108,6 @@ return {
 		{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
 		{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
 		{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = -1 }) },
+		{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
 	},
 }
